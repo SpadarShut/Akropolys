c030c48b67bebfad16b4e179e44a3d95bb53bec5
Spadar Shut <sp.shut@gmail.com>
2014-12-31 00:35:09 +0300

    404 page content

--- wordpress/wp-content/themes/akropolys/404.php
+++ wordpress/wp-content/themes/akropolys/404.php
@@ -1,7 +1,40 @@
 <?php get_header(); ?>
+<div class="main_column group">
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+	<?php get_sidebar(); ?>
 
-<?php get_sidebar(); ?>
+	<div class="main_content">
+		<div class="frameblock_top"><div class="frameblock_bot">
+			<div class="frameblock_cont group">
 
+				<h1 class="single_heading">К сожалению, такой страницы сайта не существует</h1>
+
+				<div class="" style="float:left;width: 48%;margin-right: 2em">
+
+					<p>Попробуйте поискать здесь:</p>
+
+					<ul>
+						<li><a href="http://akropolys.by/">Главная</a></li>
+						<li><a href="http://akropolys.by/product_type/karnizy/">Карнизы из гипса</a></li>
+						<li><a href="http://akropolys.by/product_type/kolonny/">Гипсовые колонны</a></li>
+						<li><a href="http://akropolys.by/product_type/rozetki/">Потолочные розетки</a></li>
+						<li><a href="http://akropolys.by/oformlenie-fasada/">Оформление фасадов</a></li>
+						<li><a href="http://akropolys.by/rospis_sten/">Роспись стен</a></li>
+						<li><a href="http://akropolys.by/kontakty/">Контакты</a></li>
+					</ul>
+				</div>
+
+
+				<p style="float: left">По всем вопросам звоните и пишите: <br/>
+					Velcom: <b>+375 (29) 321-00-85</b><br>
+					e-mail: <b><a href="mailto:info@akropolys.by">info@akropolys.by</a></b>
+				</p>
+
+
+			</div>
+		</div></div>
+
+	</div><!-- /.main_content -->
+
+</div> <!-- / .main_column -->
 <?php get_footer(); ?>
