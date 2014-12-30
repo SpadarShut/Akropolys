46e2a61851fb93fd585a794456ba9a3d32857a01
Spadar Shut <sp.shut@gmail.com>
2014-12-31 00:35:48 +0300

    formatting

--- wordpress/wp-content/themes/akropolys/page.php
+++ wordpress/wp-content/themes/akropolys/page.php
@@ -11,23 +11,21 @@
             <div class="frameblock_top"><div class="frameblock_bot">
                 <div class="frameblock_cont group">
 					
-					<div class="text_page_block group">
-					
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-						
-						
-						<div class="entry-content">
-							
-							<?php the_content(); ?>
-						
-						</div>
-						
-					<?php edit_post_link('Редактировать эту страницу','<p class="clear">','</p>'); ?>
-						
-				<?php endwhile; endif; ?>
+                    <div class="text_page_block group">
 
-					</div><!-- /.text_page_block -->
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                        <div class="entry-content">
+
+                          <?php the_content(); ?>
+
+                        </div>
+
+                        <?php edit_post_link('Редактировать эту страницу','<p class="clear">','</p>'); ?>
+
+                        <?php endwhile; endif; ?>
+
+                    </div><!-- /.text_page_block -->
 
                 </div>
             </div></div>
