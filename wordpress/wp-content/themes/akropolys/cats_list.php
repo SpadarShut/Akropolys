ab1f832909fdf31f5eaaafde9415c1ffa12a5b41
Spadar Shut <sp.shut@gmail.com>
2015-02-02 14:22:05 +0700

    fix missing quote

--- wordpress/wp-content/themes/akropolys/cats_list.php
+++ wordpress/wp-content/themes/akropolys/cats_list.php
@@ -11,7 +11,7 @@
         array(type=>'pilyastry', title=>'Пилястры', img => 'pilastry.jpg'),
         array(type=>'ornamenty', title=>'Орнаменты', img => 'ornament.jpg'),
         array(type=>'elementy-fasada', title=>'Элементы фасада', img => 'fasad.jpg'),
-        array(type=>'balyasinyi-iz-betona, title=>'Балясины', img => 'balasiny.jpg'),
+        array(type=>'balyasinyi-iz-betona', title=>'Балясины', img => 'balasiny.jpg'),
         array(type=>'kolonny-i-pilyastry', title=>'Фасадные колонны и пилястры', img => 'fasad-kolonny-pilastry.jpg'),
       );
   ?>
